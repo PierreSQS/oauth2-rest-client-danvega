48cd7ed94f1db4ad743ce11fe199f46103df9080
PierreSQS <nomo_messina@web.de>
2026-03-01 09:32:56 +0100

    upgraded SB and Auth-Server Code to 4.0.3

--- authorization-server/src/main/java/com/golfacademy/authserver/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/golfacademy/authserver/AuthorizationServerConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -14,8 +12,6 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.UUID;
@@ -41,9 +37,15 @@ public class AuthorizationServerConfig {
     }
 
     @Bean
-    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        return http.formLogin(Customizer.withDefaults()).build();
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) {
+        http
+                .oauth2AuthorizationServer(authorizationServer -> {
+                    http.securityMatcher(authorizationServer.getEndpointsMatcher());
+                    authorizationServer
+                            .oidc(Customizer.withDefaults());	// Enable OpenID Connect 1.0
+                })
+                .formLogin(Customizer.withDefaults());
+        return http.build();
     }
     
 }
